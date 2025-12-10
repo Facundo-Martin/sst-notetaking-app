e3aa66bb7efd1ac700a441912be760aad0c1f427
Facundo Martin <facundomartin98@gmail.com>
2025-12-10 08:57:35 -0300

    initial commit

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@sst-notetaking-app/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
--- sst.config.ts
+++ sst.config.ts
@@ -3,18 +3,18 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "sst-notetaking-app",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
   async run() {
-    const storage = await import("./infra/storage");
+    await import("./infra/storage");
     await import("./infra/api");
 
-    return {
-      MyBucket: storage.bucket.name,
-    };
+    // return {
+    //   MyBucket: storage.bucket.name,
+    // };
   },
 });
